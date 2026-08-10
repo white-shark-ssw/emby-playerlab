98098e082d98314d05d9417b216b0da88f9f9a3e
white-shark-ssw <houstools@gmail.com>
2026-08-11 00:32:59 +0800

    cleanup: remove obsolete SwiftUI frame compatibility shim

--- Sources/UI/SwiftUIFrameCompatibility.swift
+++
@@ -1,7 +0,0 @@
-import SwiftUI
-
-extension View {
-    func frame(width: CGFloat, minHeight: CGFloat, alignment: Alignment = .center) -> some View {
-        self.frame(width: width, alignment: alignment).frame(minHeight: minHeight, alignment: alignment)
-    }
-}
