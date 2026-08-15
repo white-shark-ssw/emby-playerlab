5230310481b6fb372891486d2232fd6e8156890b
white-shark-ssw <houstools@gmail.com>
2026-08-16 02:20:17 +0800

    Bridge picture size into AVPlayer surface

--- Sources/UI/AVPlayerSurface.swift
+++ Sources/UI/AVPlayerSurface.swift
@@ -5,23 +5,34 @@ import UIKit
 final class PlayerSurfaceUIView: UIView {
     override class var layerClass: AnyClass { AVPlayerLayer.self }
 
-    var playerLayer: AVPlayerLayer {
-        layer as! AVPlayerLayer
-    }
+    var playerLayer: AVPlayerLayer { layer as! AVPlayerLayer }
 }
 
 struct AVPlayerSurface: UIViewRepresentable {
     let player: AVPlayer
+    let scaleMode: PlayerVideoScaleMode
 
     func makeUIView(context: Context) -> PlayerSurfaceUIView {
         let view = PlayerSurfaceUIView()
         view.backgroundColor = .black
-        view.playerLayer.videoGravity = .resizeAspect
         view.playerLayer.player = player
+        applyScaleMode(scaleMode, to: view.playerLayer)
         return view
     }
 
     func updateUIView(_ uiView: PlayerSurfaceUIView, context: Context) {
         uiView.playerLayer.player = player
+        applyScaleMode(scaleMode, to: uiView.playerLayer)
+    }
+
+    private func applyScaleMode(_ mode: PlayerVideoScaleMode, to layer: AVPlayerLayer) {
+        switch mode {
+        case .fill:
+            layer.videoGravity = .resizeAspectFill
+        case .ratio16x9, .ratio4x3:
+            layer.videoGravity = .resize
+        case .fit, .source:
+            layer.videoGravity = .resizeAspect
+        }
     }
 }
