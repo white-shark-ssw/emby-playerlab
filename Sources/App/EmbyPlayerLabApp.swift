90ab963c7c43ec420cdd50820c5b2c780a8c0f6c
white-shark-ssw <houstools@gmail.com>
2026-08-15 22:58:27 +0800

    appearance: apply global system light dark preference

--- Sources/App/EmbyPlayerLabApp.swift
+++ Sources/App/EmbyPlayerLabApp.swift
@@ -3,11 +3,13 @@ import SwiftUI
 @main
 struct EmbyPlayerLabApp: App {
     @StateObject private var sessionStore = SessionStore()
+    @AppStorage(AppAppearanceSettings.interfaceStyleKey) private var interfaceStyleRaw = AppInterfaceStyle.system.rawValue
 
     var body: some Scene {
         WindowGroup {
             RootView()
                 .environmentObject(sessionStore)
+                .preferredColorScheme(AppInterfaceStyle(rawValue: interfaceStyleRaw)?.colorScheme)
         }
     }
 }
