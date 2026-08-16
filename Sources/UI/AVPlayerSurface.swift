a59989a88ebebcb568ba88dd090a24bda2a14b8b
white-shark-ssw <houstools@gmail.com>
2026-08-16 23:42:20 +0800

    Apply shared video layout plan to AVPlayer surface

--- Sources/UI/AVPlayerSurface.swift
+++ Sources/UI/AVPlayerSurface.swift
@@ -10,12 +10,12 @@ final class PlayerSurfaceUIView: UIView {
 
 struct AVPlayerSurface: UIViewRepresentable {
     let player: AVPlayer
-    let scaleMode: PlayerVideoScaleMode
+    let layoutPlan: VideoLayoutPlan
     let onPlayerLayerReady: ((AVPlayerLayer) -> Void)?
 
-    init(player: AVPlayer, scaleMode: PlayerVideoScaleMode = .fit, onPlayerLayerReady: ((AVPlayerLayer) -> Void)? = nil) {
+    init(player: AVPlayer, layoutPlan: VideoLayoutPlan, onPlayerLayerReady: ((AVPlayerLayer) -> Void)? = nil) {
         self.player = player
-        self.scaleMode = scaleMode
+        self.layoutPlan = layoutPlan
         self.onPlayerLayerReady = onPlayerLayerReady
     }
 
@@ -23,22 +23,22 @@ struct AVPlayerSurface: UIViewRepresentable {
         let view = PlayerSurfaceUIView()
         view.backgroundColor = .black
         view.playerLayer.player = player
-        applyScaleMode(scaleMode, to: view.playerLayer)
+        applyLayout(layoutPlan, to: view.playerLayer)
         DispatchQueue.main.async { onPlayerLayerReady?(view.playerLayer) }
         return view
     }
 
     func updateUIView(_ uiView: PlayerSurfaceUIView, context: Context) {
         uiView.playerLayer.player = player
-        applyScaleMode(scaleMode, to: uiView.playerLayer)
+        applyLayout(layoutPlan, to: uiView.playerLayer)
         DispatchQueue.main.async { onPlayerLayerReady?(uiView.playerLayer) }
     }
 
-    private func applyScaleMode(_ mode: PlayerVideoScaleMode, to layer: AVPlayerLayer) {
-        switch mode {
-        case .fill: layer.videoGravity = .resizeAspectFill
-        case .ratio16x9, .ratio4x3: layer.videoGravity = .resize
-        case .fit, .source: layer.videoGravity = .resizeAspect
+    private func applyLayout(_ plan: VideoLayoutPlan, to layer: AVPlayerLayer) {
+        switch plan.contentMode {
+        case .aspectFit: layer.videoGravity = .resizeAspect
+        case .aspectFill: layer.videoGravity = .resizeAspectFill
+        case .stretch: layer.videoGravity = .resize
         }
     }
 }
