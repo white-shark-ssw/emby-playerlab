f98752e8ebaf347efda29eaeed9add15eab23990
white-shark-ssw <houstools@gmail.com>
2026-08-23 20:59:16 +0800

    perf: arm one-megabyte startup range bootstrap

--- Sources/Transport/PlaybackTransportContext.swift
+++ Sources/Transport/PlaybackTransportContext.swift
@@ -40,6 +40,9 @@ final class PlaybackTransportContext: @unchecked Sendable {
     init(source: ResolvedPlaybackSource, client: EmbyAPIClient, configuration: MediaTransportConfiguration) {
         let starvationState = PlaybackTransportStarvationState()
         self.starvationState = starvationState
+        let cacheKey = TransportCacheMaintenance.stableVideoCacheKey(for: source)
+        StartupRangeBootstrapCache.shared.arm(cacheKey: cacheKey, maximumBytes: 1 * 1_048_576)
+        DiagnosticsLogger.shared.playback("StartupBootstrap", "armed bytes=1048576 item=\(source.itemId)")
         session = UnifiedMediaTransportSession(source: source, configuration: configuration, starvationState: starvationState)
         _ = client
     }
