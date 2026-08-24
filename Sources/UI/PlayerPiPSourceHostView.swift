11c6813c0d9900da4713dc6f0be39695948f68fe
white-shark-ssw <houstools@gmail.com>
2026-08-24 20:47:51 +0800

    Build168: remove PiP placeholder from source host handoff

--- Sources/UI/PlayerPiPSourceHostView.swift
+++ Sources/UI/PlayerPiPSourceHostView.swift
@@ -3,30 +3,22 @@ import UIKit
 
 final class PlayerPiPSourceHostView: UIView {
     let displayLayer = AVSampleBufferDisplayLayer()
-    private let placeholderImageView = UIImageView(image: UIImage(systemName: "pip"))
 
     override init(frame: CGRect) {
         super.init(frame: frame)
-        backgroundColor = .black
+        backgroundColor = .clear
+        isOpaque = false
         isUserInteractionEnabled = false
         clipsToBounds = true
-        placeholderImageView.tintColor = UIColor.white.withAlphaComponent(0.55)
-        placeholderImageView.contentMode = .scaleAspectFit
-        addSubview(placeholderImageView)
         layer.addSublayer(displayLayer)
     }
 
     required init?(coder: NSCoder) { fatalError("init(coder:) has not been implemented") }
 
-    func markVideoAvailable() {
-        placeholderImageView.isHidden = true
-        backgroundColor = .clear
-    }
+    func markVideoAvailable() {}
 
     override func layoutSubviews() {
         super.layoutSubviews()
-        let iconSide = min(max(min(bounds.width, bounds.height) * 0.18, 56), 110)
-        placeholderImageView.frame = CGRect(x: bounds.midX - iconSide * 0.5, y: bounds.midY - iconSide * 0.5, width: iconSide, height: iconSide)
         guard displayLayer.superlayer === layer else { return }
         CATransaction.begin(); CATransaction.setDisableActions(true); displayLayer.frame = bounds; displayLayer.contentsScale = window?.screen.nativeScale ?? UIScreen.main.nativeScale; CATransaction.commit()
     }
