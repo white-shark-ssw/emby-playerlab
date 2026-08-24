643e708488faa90f6b1b15d899e8cb1844cb242c
white-shark-ssw <houstools@gmail.com>
2026-08-24 18:38:45 +0800

    Build165: expose MPV seek dispatch telemetry to PiP

--- Sources/UI/PlayerPiPInlineRendererAdapter.swift
+++ Sources/UI/PlayerPiPInlineRendererAdapter.swift
@@ -12,6 +12,20 @@ extension PlayerPiPInlineRendererControlling {
     }
 }
 
+struct PlayerPiPSeekDispatchInfo: Sendable {
+    let seekID: UInt64
+    let requestedTarget: Double
+    let dispatchTarget: Double
+    let previousKeyframe: Double?
+    let nextKeyframe: Double?
+
+    var stagingTarget: Double {
+        if abs(dispatchTarget - requestedTarget) > 0.001 { return dispatchTarget }
+        return previousKeyframe ?? requestedTarget
+    }
+}
+
 protocol PlayerPiPSeekLandingProviding: AnyObject {
+    var pictureInPictureSeekDispatchHandler: ((PlayerPiPSeekDispatchInfo) -> Void)? { get set }
     var pictureInPictureSeekLandingHandler: ((SeekResult) -> Void)? { get set }
 }
