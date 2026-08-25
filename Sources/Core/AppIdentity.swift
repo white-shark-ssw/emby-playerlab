c97d75daab5567c1d77ebae9e59a3705636068d6
white-shark-ssw <houstools@gmail.com>
2026-08-25 17:59:33 +0800

    Set Build177 carousel test identity

--- Sources/Core/AppIdentity.swift
+++ Sources/Core/AppIdentity.swift
@@ -3,8 +3,8 @@ import Foundation
 
 enum AppIdentity {
     static let clientName = "OnePlayer"
-    static let sourceVersion = "0.14.6"
-    static let version = Bundle.main.object(forInfoDictionaryKey: "CFBundleShortVersionString") as? String ?? "0.14.6"
+    static let sourceVersion = "0.14.10"
+    static let version = Bundle.main.object(forInfoDictionaryKey: "CFBundleShortVersionString") as? String ?? "0.14.10"
     static let ticksPerSecond: Double = 10_000_000
 
     static var deviceName: String {
