842b56ab4c72fb89efa168f837c2c541aa9577cd
white-shark-ssw <houstools@gmail.com>
2026-08-27 22:21:10 +0800

    Move poster candidate to Build220

--- Sources/Core/AppIdentity.swift
+++ Sources/Core/AppIdentity.swift
@@ -3,8 +3,8 @@ import Foundation
 
 enum AppIdentity {
     static let clientName = "OnePlayer"
-    static let sourceVersion = "0.14.52"
-    static let version = Bundle.main.object(forInfoDictionaryKey: "CFBundleShortVersionString") as? String ?? "0.14.52"
+    static let sourceVersion = "0.14.53"
+    static let version = Bundle.main.object(forInfoDictionaryKey: "CFBundleShortVersionString") as? String ?? "0.14.53"
     static let ticksPerSecond: Double = 10_000_000
 
     static var deviceName: String {
