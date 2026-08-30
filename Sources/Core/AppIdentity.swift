d2b2696a60d11a2da530e85c8258957240c7aa1c
white-shark-ssw <houstools@gmail.com>
2026-08-30 22:48:29 +0800

    Build265: set carousel analysis A/B identity

--- Sources/Core/AppIdentity.swift
+++ Sources/Core/AppIdentity.swift
@@ -3,8 +3,8 @@ import Foundation
 
 enum AppIdentity {
     static let clientName = "OnePlayer"
-    static let sourceVersion = "0.14.97"
-    static let version = Bundle.main.object(forInfoDictionaryKey: "CFBundleShortVersionString") as? String ?? "0.14.97"
+    static let sourceVersion = "0.14.98"
+    static let version = Bundle.main.object(forInfoDictionaryKey: "CFBundleShortVersionString") as? String ?? "0.14.98"
     static let ticksPerSecond: Double = 10_000_000
 
     static var deviceName: String {
